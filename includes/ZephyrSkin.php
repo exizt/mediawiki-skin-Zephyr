3f147e997eaef37c7acf69937e490970f7ad8327
exizt <e2xist@gmail.com>
2024-01-04 03:41:12 +0900

    bug fix

--- includes/ZephyrSkin.php
+++ includes/ZephyrSkin.php
@@ -14,14 +14,17 @@ class ZephyrSkin extends SkinMustache {
         $data['html-myskin-hello-world'] = '<strong>Hello world!</strong>'; // or $this->msg('msg-key')->parse();
         // $data['html-myskin-hello-world'] = print_r($data, true);
 
-        // $pageTitle = $this->getTitle()->prefixedText;
-        $pageTitle = $this->getTitle()->getPrefixedUrl();
-        $watchHtml = '<a href="/w/index.php?title='.$pageTitle.'&amp;action=watch" class="mw-watchlink" data-mw="interface" title="이 문서를 주시문서 목록에 추가 [w]" accesskey="w">주시</a>';
-        $data['html-action-watch'] = $watchHtml;
+        if($data['is-article'] && !$data['is-anon']){
+            // $pageTitle = $this->getTitle()->prefixedText;
+            $pageTitle = $this->getTitle()->getPrefixedUrl();
+            $watchHtml = '<a href="/w/index.php?title='.$pageTitle.'&amp;action=watch" class="mw-watchlink" data-mw="interface" title="이 문서를 주시문서 목록에 추가 [w]" accesskey="w">주시</a>';
+            $data['html-action-watch'] = $watchHtml;
+        } else {
+            $data['html-action-watch'] = '';
+        }
 
-        $debug = $this->getTitle()->prefixedText;
-        $data['html-myskin-hello-world'] = print_r($debug, true);
-
+        // $debug = $this->getTitle()->prefixedText;
+        // $data['html-myskin-hello-world'] = print_r($debug, true);
 
         return $data;
     }
